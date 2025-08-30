51366946de6159400f0ae791a9e8c354dfd64e57
shubhitiwari13 <shubhitiwari013@gmail.com>
2025-08-30 16:01:30 +0530

    Add files via upload

---
+++ practical17.php
@@ -0,0 +1,7 @@
+<?php
+echo "Odd numbers between 20 and 80 are:\n";
+
+for ($i = 21; $i < 80; $i += 2) {
+    echo $i . " ";
+}
+?>
---
+++ practical18.php
@@ -0,0 +1,7 @@
+<?php
+echo "Even numbers from 1 to 100 are:\n";
+
+for ($i = 2; $i <= 100; $i += 2) {
+    echo $i . " ";
+}
+?>
